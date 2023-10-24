aa1ec55fc89f8183e5e928a415f26026697136b1
NikDoe <nikd0e@yandex.ru>
2023-10-24 16:11:41 +0300

    add loading statement w/ spinner

--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,10 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './Header';
 import CartOverview from '../features/cart/CartOverview';
+import Spinner from './Spinner';
 
 function AppLayout() {
+	const { state } = useNavigation();
+	const isLoading = state === 'loading';
+
 	return (
-		<div>
+		<div className='layout'>
+			{isLoading && <Spinner />}
 			<Header />
 			<main>
 				<Outlet />
